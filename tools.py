79205fab2512e1b4e7e4ddd690eaf216a45eb6bd
Mathias Palm <mathias@iup.uni-bremen.de>
2021-08-06 17:34:39 +0200

    vbugfix in average

--- tools.py
+++ tools.py
@@ -18,22 +18,24 @@ def average(flist, av_spc=False, av_ifg=False):
         nr = 1
         
     for ff in flist:
-        sp = ftsreader(flist[0], getspc=av_spc, getifg=av_ifg)
+        sp = ftsreader(ff, getspc=av_spc, getifg=av_ifg)
         if np.any (spcwvn != sp.spcwvn):
             print('Different wavenumbers')
             return(-1)
         spc += sp.spc
         nr += 1
-        print(spc,nr)
+
 
     spc /= nr
+    print (np.std(spc[100:]))
 
     return(np.vstack((spcwvn,spc)).T)
         
-def divide_spectra(spec1, spec2, interpolate=False):
+def divide_spectra(spec1, spec2, interpolate=False, normalise=True):
     # calculates spec1/spec2
     # spec = [wavenumber, spectrum]
     # if interpolate = True, spec2 gets interpolated to the wavenumber of spec1
+    # if mormailze = True, normailise the spectrum to be between 0 and 1
 
     if interpolate:
         spec2_ip = spec1.copy()
@@ -45,5 +47,12 @@ def divide_spectra(spec1, spec2, interpolate=False):
 
     print(spec1[:,1])
     spec [:,1] = (spec1[:,1]/spec2_ip[:,1]).copy()
+
+    if normalise:
+        spec[:,1] /= np.mean(spec[:,1])
     
     return(spec)
+
+def save_spectrum(spec, filename):
+    # saves the spectrum in spec to file <filename>
+    np.savetxt(filename, spec, delimiter=' ')
